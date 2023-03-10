c1c089e384a04e8e79a636e670ef9d6a42a3c0f6
jay <280075924@qq.com>
2023-03-10 15:49:58 +0800

    first

--- blockchain.py
+++ blockchain.py
@@ -19,11 +19,11 @@ from flask import Flask
 
 class Blockchain:
     def __init__(self):
-        self.chain= []
+        self.chain = []
         self.current_transactions = []
         self.new_block(proof=100, previous_hash=1)
 
-    def new_block(self,proof,previous_hash = None):
+    def new_block(self, proof, previous_hash=None):
         block = {
             "index": len(self.chain) + 1,
             "timestamp": time(),
@@ -35,14 +35,13 @@ class Blockchain:
         self.chain.append(block)
         return block
 
-
     def new_transaction(self, sender, recipient, amount):
         self.current_transactions.append(
             {
                 "sender": sender,
                 "recipient": recipient,
                 "amount": amount
-             }
+            }
         )
         return self.last_block['index'] + 1
 
@@ -63,11 +62,13 @@ class Blockchain:
         print(proof)
         return proof
 
+    @staticmethod
     def valid_proof(self, last_proof: int, proof: int) -> bool:
         guess = f'{last_proof}{proof}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
         print(guess_hash)
         return guess_hash[0:4] == "0000"
+
 
 app = Flask(__name__)
 if __name__ == '__main__':
